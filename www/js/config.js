46ae0376473e9d3ad90a1dce7bff651d6c6ee99d
Ankit Aggarwal <ankit@eglogics.com>
2018-04-18 14:11:15 +0530

    Initial Commit
    Setup

---
+++ www/js/config.js
@@ -0,0 +1,4 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'https://www.realtymyths.com/api/')
+.constant('WORDPRESS_PUSH_URL', 'https://www.realtymyths.com/api/')
+.constant('GCM_SENDER_ID', '574597432927');
